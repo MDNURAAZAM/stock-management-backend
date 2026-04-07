6f6edc8de2a17178435ca81861118965b201d799
mdnuraazam <md.nura.azam@g.bracu.ac.bd>
2026-04-07 23:17:57 +0600

    add get route for products table

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from models import Product
 from sqlalchemy import select
+from typing import List
+from schemas.product import ProductRead, ProductCreate, ProductUpdate
+from database import get_db
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
@@ -18,3 +21,23 @@ def get_product(session: Session, product_id: int) -> Product:
             status_code=404, detail=f"Product with ID {product_id} not found"
         )
     return result[0]
+
+
+# -------------
+# FETCH all products
+# -------------
+@router.get("/", response_model=List[ProductRead])
+def get_products(session: Session = Depends(get_db)):
+    query = select(Product)
+    result = session.execute(query).all()
+
+    return [
+        {
+            "id": product.id,
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "quantity": product.quantity,
+        }
+        for product in result
+    ]
